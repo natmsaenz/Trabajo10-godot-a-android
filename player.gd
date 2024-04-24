29f14cc9e96f197062cfb23b2b6f81721cdccf63
natmsaenz <natalia.madrigal2941@alumnos.udg.mx>
2024-04-23 20:43:16 -0600

    Escena inicial empezada, creación de nodos de la misma

--- player.gd
+++ player.gd
@@ -43,13 +43,13 @@ func _process(delta):
 	
 
 
-func _on_body_entered(body):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
-
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+
+func _on_body_entered(_body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
