efb284b76e037c771d2ee20226bb14262c944406
natmsaenz <natalia.madrigal2941@alumnos.udg.mx>
2024-04-23 22:15:29 -0600

    Reinicio de nodo y escena Main

--- player.gd
+++ player.gd
@@ -53,3 +53,6 @@ func _on_body_entered(_body):
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
+
+
+
